5b8ba2d190b1e768903e631aae11a073ea2eaaf8
Kevin Turkington <turkingtonkevin@gmail.com>
2017-02-24 14:26:12 -0800

    command obj

--- Assignment_3/smallsh.c
+++ Assignment_3/smallsh.c
@@ -3,6 +3,16 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+
+struct InputObj
+{
+    bool Background;
+    char InputFile[256];
+    char OutputFile[256];
+    char Command[1028];
+    char *Arguments[128];
+};
 
 int ChangeDirectorysFromHome(char* InputBuffer)
 {
@@ -14,6 +24,7 @@ int ChangeDirectorysFromHome(char* InputBuffer)
     strtok(InputBuffer," ");
     strcpy(InputBuffer,strtok(NULL,""));
     InputBuffer[strlen(InputBuffer) -1] = '\0';
+
     //printf("    %s\n",InputBuffer);
     if(InputBuffer[0] == '/'){
         sprintf(NewPath,"%s%s",HomeDirectoryPath,InputBuffer);
@@ -39,6 +50,68 @@ int ChangeDirectorysFromHome(char* InputBuffer)
     return 0;
 }
 
+struct InputObj ParseInput(char* InputBuffer)
+{
+    struct InputObj *Obj = malloc(1 * sizeof(struct InputObj));
+    char Buffer[1028];
+    char *InputFileName;
+    char *OutputFileName;
+
+    InputBuffer[strlen(InputBuffer) -1] = '\0';
+
+    if(InputBuffer[strlen(InputBuffer) -1] == '&'){
+        Obj->Background = true;
+        printf("Background: enabled\n");
+    }
+    else{
+        Obj->Background = false;
+        printf("Background: disabled\n");
+    }
+
+    //command
+    memset(Buffer,'\0',sizeof(Buffer));
+    strcpy(Buffer,InputBuffer);
+    strtok(Buffer," ");
+    strcpy(Obj->Command,Buffer);
+    printf("Command: %s\n",Obj->Command);
+
+    //InputFile Name
+    memset(Buffer,'\0',sizeof(Buffer));
+    strcpy(Buffer,InputBuffer);
+    InputFileName = strstr(Buffer,"<");
+    if(InputFileName != NULL){
+        memmove(InputFileName, InputFileName+2, strlen(InputFileName));
+        strtok(InputFileName," ");
+        InputFileName[strlen(InputFileName)] = '\0';
+        strcpy(Obj->InputFile,InputFileName);
+        printf("Inputfile is:%s\n",Obj->InputFile);
+    }
+
+    //OutputFile Name;
+    memset(Buffer,'\0',sizeof(Buffer));
+    strcpy(Buffer,InputBuffer);
+    OutputFileName = strstr(Buffer,">");
+    if(OutputFileName != NULL){
+        memmove(OutputFileName, OutputFileName+2, strlen(OutputFileName));
+        strtok(OutputFileName," ");
+        OutputFileName[strlen(OutputFileName)] = '\0';
+        strcpy(Obj->OutputFile,OutputFileName);
+        printf("Outputfile is:%s\n",Obj->OutputFile);
+    }
+     
+    //arguments
+    memset(Buffer,'\0',sizeof(Buffer));
+    strcpy(Buffer,InputBuffer);
+
+    return *Obj;
+
+} 
+
+int RunCommand(char* InputBuffer)
+{
+    return 0;
+}
+
 void RunShell()
 {
     char InputBuffer[1028];
@@ -59,7 +132,7 @@ void RunShell()
         else if(strncmp(InputBuffer, "#",1) == 0){
             //printf("Comment Comment Comment \n");
         }
-        else if(strncmp(InputBuffer,"cd", 2) == 0){
+        else if(strncmp(InputBuffer,"cd ", 3) == 0){
             //printf("Changing directory...\n");
             ChangeDirectorysFromHome(InputBuffer);
         }
@@ -68,6 +141,7 @@ void RunShell()
         }
         else{
             //read in a command.
+            ParseInput(InputBuffer);
         }
     }
     while(true);
