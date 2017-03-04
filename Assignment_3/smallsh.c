152350bb49e1a14647d722335661aec0242d97de
Kevin Turkington <turkingtonkevin@gmail.com>
2017-03-04 15:44:09 -0800

    ready for testing

--- Assignment_3/smallsh.c
+++ Assignment_3/smallsh.c
@@ -1,3 +1,10 @@
+/***************************************
+** Kevin Turkington
+** CS344
+** Assignment 3 - smallsh
+** 3/4/17
+***************************************/
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
@@ -8,9 +15,12 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <errno.h>
 
-#define STACKMAX 64
+#define STACKMAX 512
 
+/// NAME: InputObj
+/// DESC: contains a parsed line all in one struct.
 struct InputObj
 {
     bool Background;
@@ -21,6 +31,9 @@ struct InputObj
     char *Arguments[128];
 };
 
+
+/// NAME: PidStackObj
+/// DESC: modified stack for managing background processes
 struct PidStackObj
 {
     int NumBackPid;
@@ -33,6 +46,9 @@ pid_t LastForeGroundStatus;
 bool ForegroundOnly = false;
 //End Globals
 
+
+/// NAME: _InitPidObj
+/// DESC: Creates pid stack with -1 in each val.
 void _InitPidObj()
 {
     int i;
@@ -43,78 +59,114 @@ void _InitPidObj()
     }
 }
 
+
+/// NAME: PushBackPid
+/// DESC: adds a pid to the stack
 void PushBackPid(pid_t processId)
 {
     PidStack.BackgroundPids[++(PidStack.NumBackPid)] = processId;
 }
 
+/// NAME: RemoveBackPid
+/// DESC: removes a pid if an earlier bg process ends.
+void RemoveBackPid(pid_t processId)
+{
+    int i;
+    int pidPos;
+    for(i = 0; i < PidStack.NumBackPid + 1;i++){
+        if(PidStack.BackgroundPids[i] == processId){
+            pidPos = i;
+            break;
+        }
+    }
+
+    for(i = pidPos; i < PidStack.NumBackPid + 1;i++){
+        PidStack.BackgroundPids[i] = PidStack.BackgroundPids[i+1];
+    }
+    
+    PidStack.NumBackPid--;
+
+}
+
+/// NAME: PopBackPid
+/// DESC: removes pid from top
 pid_t PopBackPid()
 {
     return PidStack.BackgroundPids[PidStack.NumBackPid--];
 }
 
+/// NAME: TopBackPid
+/// DESC: reads top of stack
 pid_t TopBackPid()
 {
     return PidStack.BackgroundPids[PidStack.NumBackPid];
 }
 
+/// NAME: ChangeDirectorysFromHome
+/// DESC: allows user to navigate file directory
 int ChangeDirectorysFromHome(char* InputBuffer)
 {
-    char* HomeDirectoryPath = getenv("HOME");
+    char* HomeDirectoryPath = getenv("HOME"); //gets home path.
     char NewPath[1028];
 
     memset(NewPath,'\0',sizeof(NewPath));
 
-    strtok(InputBuffer," ");
+    strtok(InputBuffer," "); // removing unnessary spacing
     strcpy(InputBuffer,strtok(NULL,""));
     InputBuffer[strlen(InputBuffer) -1] = '\0';
 
     //printf("    %s\n",InputBuffer);
     if(InputBuffer[0] == '/'){
-        sprintf(NewPath,"%s%s",HomeDirectoryPath,InputBuffer);
+        sprintf(NewPath,"%s%s",HomeDirectoryPath,InputBuffer); // goto a specifed directory from home directory
         //printf("Path: %s\n",NewPath);
     }
-    else if(strcmp(InputBuffer,"..") == 0){
+    else if(strcmp(InputBuffer,"..") == 0){ // go back a folder
         strcpy(NewPath,InputBuffer);
         //printf("Path: %s\n",NewPath);
     }
-    else if(InputBuffer[0] == '.' && InputBuffer[1] == '/'){
+    else if(InputBuffer[0] == '.' && InputBuffer[1] == '/'){ // current directory
         sprintf(NewPath,"%s",InputBuffer);
         //printf("Path: %s\n",NewPath);
     }
     else{
-        sprintf(NewPath,"%s/%s",HomeDirectoryPath,InputBuffer);
+        sprintf(NewPath,"%s/%s",HomeDirectoryPath,InputBuffer); // goto directory from home
         //printf("Path: %s\n",NewPath);
     }
-
-    if(chdir(NewPath) != 0){
+    if(chdir(NewPath) != 0){ // cannot find directory
         printf("Directory:%s not found.\n",NewPath);
         return 1;
     }
     return 0;
 }
 
+/// NAME: _IsLeadCharSpecial
+/// DESC: Helper function to check for &<> and prevent additonal args
 bool _IsLeadCharSpecial(char *str)
 {
     bool IsSpecial = false;
 
-    if(str == NULL){
+    if(str == NULL){ // make sure value isnt null so rest of conditions dont seg fault.
         return true;
     }
 
-    if(str[0] == '&'){
+    if(str[0] == '&'){ // check for bg.
         IsSpecial = true;
     }
-    else if(str[0] == '<'){
+    else if(str[0] == '<'){ // check for input char
         IsSpecial = true;
     }
-    else if(str[0] == '>'){
+    else if(str[0] == '>'){ // check for output char
+        IsSpecial = true;
+    }
+    else if(str[0] == '#'){ // check for command char
         IsSpecial = true;
     }
 
     return IsSpecial;
 }
 
+/// NAME: ParseInput
+/// DESC: inits struct that holds all args.
 void ParseInput(char* InputBuffer,struct InputObj* Obj)
 {
     //struct InputObj Obj = malloc(1 * sizeof(struct InputObj));
@@ -125,11 +177,11 @@ void ParseInput(char* InputBuffer,struct InputObj* Obj)
     //int i;
 
     Obj->NumArgs = 0;
-    InputBuffer[strlen(InputBuffer) -1] = '\0';
+    InputBuffer[strlen(InputBuffer) -1] = '\0'; // removed \n
 
-    if(InputBuffer[strlen(InputBuffer) -1] == '&'){
+    if(InputBuffer[strlen(InputBuffer) -1] == '&'){ // check for bg enabled
         Obj->Background = true;
-        InputBuffer[strlen(InputBuffer) -1] = '\0';
+        InputBuffer[strlen(InputBuffer) -1] = '\0'; // remove char.
         //printf("Background: enabled\n");
     }
     else{
@@ -145,13 +197,13 @@ void ParseInput(char* InputBuffer,struct InputObj* Obj)
     //printf("Command: %s\n",Obj->Command);
 
     //InputFile Name
-    memset(Buffer,'\0',sizeof(Buffer));
+    memset(Buffer,'\0',sizeof(Buffer)); 
     strcpy(Buffer,InputBuffer);
-    InputFileName = strstr(Buffer,"<");
+    InputFileName = strstr(Buffer,"<"); // grab everything after <
     if(InputFileName != NULL){
-        memmove(InputFileName, InputFileName+2, strlen(InputFileName));
-        strtok(InputFileName," ");
-        InputFileName[strlen(InputFileName)] = '\0';
+        memmove(InputFileName, InputFileName+2, strlen(InputFileName)); //place everything but "< " in string
+        strtok(InputFileName," "); // cut off excess
+        InputFileName[strlen(InputFileName)] = '\0'; // add end char
         strcpy(Obj->InputFile,InputFileName);
         //printf("Inputfile is:%s\n",Obj->InputFile);
     }
@@ -159,41 +211,38 @@ void ParseInput(char* InputBuffer,struct InputObj* Obj)
     //OutputFile Name;
     memset(Buffer,'\0',sizeof(Buffer));
     strcpy(Buffer,InputBuffer);
-    OutputFileName = strstr(Buffer,">");
+    OutputFileName = strstr(Buffer,">"); // grab everything after >
     if(OutputFileName != NULL){
-        memmove(OutputFileName, OutputFileName+2, strlen(OutputFileName));
-        strtok(OutputFileName," ");
-        OutputFileName[strlen(OutputFileName)] = '\0';
+        memmove(OutputFileName, OutputFileName+2, strlen(OutputFileName));//place everything but "> " in string
+        strtok(OutputFileName," "); // cut off excess
+        OutputFileName[strlen(OutputFileName)] = '\0'; // add end char
         strcpy(Obj->OutputFile,OutputFileName);
         //printf("Outputfile is:%s\n",Obj->OutputFile);
     }
      
-     //TODO FIX
     //arguments
     memset(Buffer,'\0',sizeof(Buffer));
     strcpy(Buffer,InputBuffer);
-    strtok(Buffer," ");
+    strtok(Buffer," "); // everything before first space
 
-    Temp = strtok(NULL,"");
+    Temp = strtok(NULL,""); // grab everyhting after first space.
 
     //printf("Temp Line:%s\n",Temp);
-    if(_IsLeadCharSpecial(Temp) == false && Temp != NULL ){ // && _IsLeadCharSpecial(Buffer) == false
+    if(_IsLeadCharSpecial(Temp) == false){ // check if there are any args
         strcpy(Buffer,Temp);
-        strtok(Buffer,"<>&#");
-
+        strtok(Buffer,"<>&#"); // grab everything before args.
         
-        
-        strtok(Buffer," ");
-        Obj->Arguments[0] = Buffer;
+        strtok(Buffer," "); // cut space
+        Obj->Arguments[0] = Buffer; // first arg.
         Obj->NumArgs = 1;
-        Temp = strtok(NULL," ");
+        Temp = strtok(NULL," "); // next arg
         while(Temp != NULL){
             
-            Obj->Arguments[Obj->NumArgs] = Temp;
+            Obj->Arguments[Obj->NumArgs] = Temp;// input all args into list.
             Obj->NumArgs++;
             Temp = strtok(NULL," ");
         }
-        Obj->Arguments[Obj->NumArgs] = strtok(NULL, "");
+        Obj->Arguments[Obj->NumArgs] = strtok(NULL, ""); // grab last arg.
            
     }
     
@@ -203,51 +252,54 @@ void ParseInput(char* InputBuffer,struct InputObj* Obj)
     // }
 }
 
+/// NAME: _InitArgList
+/// DESC: creates list of args for execvp
 void _InitArgList(struct InputObj* Obj,char** Args)
 {
     int i;
 
-    Args[0] = Obj->Command;
+    Args[0] = Obj->Command; // first arg is command itself.
     for(i = 0;i < Obj->NumArgs ;i++){
-        Args[i+1] = Obj->Arguments[i];
+        Args[i+1] = Obj->Arguments[i]; // add all args.
     }
 
-    Args[i+1] = NULL;
+    Args[i+1] = NULL; // NEED THIS TO WORK.
 }
 
+/// NAME: SetupRedirects
+/// DESC: creates redirection of input and output
 void SetupRedirects(struct InputObj* Obj)
 {
     int InputFileDescriptor = STDIN_FILENO;
     int OutputFileDescriptor = STDOUT_FILENO;
 
-    if(Obj->InputFile[0] != '\0'){
+    if(Obj->InputFile[0] != '\0'){ // check if inputs are active.
         //printf("INPUT: %s\n",Obj->InputFile);
-        InputFileDescriptor = open(Obj->InputFile,O_RDONLY);
+        InputFileDescriptor = open(Obj->InputFile,O_RDONLY); // open file.
 
-        if(InputFileDescriptor < 0){
-            //perror("Cannot open input file.\n");
+        if(InputFileDescriptor < 0){ // if not found exit.
             exit(1);
         }
-
-        dup2(InputFileDescriptor,0);
-        close(InputFileDescriptor);
+        dup2(InputFileDescriptor,0); // change input redirection.
+        close(InputFileDescriptor); // close file.
     }
-    if(Obj->OutputFile[0] != '\0'){
+    if(Obj->OutputFile[0] != '\0'){ // check if outputs actve.
         //printf("OUTPUT: %s\n",Obj->OutputFile);
-        
         // Can also use creat() func
-        OutputFileDescriptor = open(Obj->OutputFile,O_WRONLY | O_CREAT | O_TRUNC,0644);
+        OutputFileDescriptor = open(Obj->OutputFile,O_WRONLY | O_CREAT | O_TRUNC,0644); // create new file or edit.
 
-        if(OutputFileDescriptor < 0){
-            //perror("Cannot create or open ouput file.\n");
+        if(OutputFileDescriptor < 0){ // check for error.
+            //perror("smallsh");
             exit(1);
         }
 
-        dup2(OutputFileDescriptor,1);
+        dup2(OutputFileDescriptor,1);//change output directions.
         close(OutputFileDescriptor);
     }
 }
 
+/// NAME: RunCommand
+/// DESC: creates a child fork for a command.
 void RunCommand(struct InputObj* Obj)
 {
     pid_t pid = fork();
@@ -265,23 +317,19 @@ void RunCommand(struct InputObj* Obj)
             SetupRedirects(Obj);
 
             _InitArgList(Obj,ArgList);
-            execvp(Obj->Command, ArgList);
-            
-            fflush(stderr);
-            //perror("Could not find command\n");
-            
-
+            execvp(Obj->Command, ArgList); // run command.
+            //perror("smallsh");
             exit(1);
             break;
 
         default: // Parent
-            if(Obj->Background == true && ForegroundOnly == false){
-                PushBackPid(pid);
+            if(Obj->Background == true && ForegroundOnly == false){ //setup bg or non bg.
+                PushBackPid(pid); // add to bg stack.
                 printf("Background Pid is %d\n",TopBackPid());
             }
             else{
                 
-                waitpid(pid,&ProcessStatus,0);
+                waitpid(pid,&ProcessStatus,0); // hang the shell is bg inactive.
                 LastForeGroundStatus = ProcessStatus;
                 //printf("parent(%d) waited for child process(%d)\n",getpid() ,pid);
             }
@@ -289,50 +337,53 @@ void RunCommand(struct InputObj* Obj)
     }
 }
 
-// SOURCE: http://stackoverflow.com/questions/2377811/tracking-the-death-of-a-child-process
-bool _BackGroundPidCompleted()
-{
-    int ChildStatus;
-    int i;
-
-    bool BGProcessReturned = false;
-
-    for(i = 0; i < PidStack.NumBackPid + 1; i++){
-        waitpid(PidStack.BackgroundPids[i], &ChildStatus, WNOHANG);
-
-        if(WEXITSTATUS(ChildStatus)){
-            printf("Background pid %d is done: exit value %d\n",PopBackPid(),WEXITSTATUS(ChildStatus));
-            fflush(stdout);
-            fflush(stdin);
-
-            BGProcessReturned = true;
-        }
-        else{
-            printf("Background pid %d is done: terminated by signal %d\n", PopBackPid(), WTERMSIG(ChildStatus));
-            fflush(stdout);
-            fflush(stdin);
-
-            BGProcessReturned = true;
-        }
-    }
-    return BGProcessReturned;
-}
-
+/// NAME: TrapStopSignal
+/// DESC: signal handler for ^z
 //SOURCE: https://piazza.com/class/ixhzh3rn2la6vk?cid=365
 void TrapStopSignal(int sig)
 {
     if(ForegroundOnly == false){
-        char* message = ("\nEntering foreground-only mode (& is now ignored)\n");
+        char* message = ("\nEntering foreground-only mode (& is now ignored)\n"); // enable Fg mode.
         write(STDOUT_FILENO, message, 50);
-        ForegroundOnly = true;
+        ForegroundOnly = true; // change global.
     }
     else{
-        char* message = "\nExiting foreground-only mode\n";
+        char* message = "\nExiting foreground-only mode\n"; // exit Fg mode.
         write(STDOUT_FILENO, message, 31);
-        ForegroundOnly = false;
+        ForegroundOnly = false; // change gloabl.
     }
 }
 
+/// NAME: TrapChildSignal
+/// DESC: signal handler for a child process ending.
+/// I cannot use a function to check what signal is which else TrapChildSignal is exited.
+// SOURCE: http://stackoverflow.com/questions/2377811/tracking-the-death-of-a-child-process
+void TrapChildSignal(int sig)
+{
+    pid_t ChildPid;
+    int ChildStatus;
+    int i;
+
+    //while( (ChildPid = waitpid(-1,&ChildStatus,WNOHANG)) != -1 ){
+    for(i = 0;i < PidStack.NumBackPid + 1;i++){ // find pid that exited.
+        ChildPid = waitpid(PidStack.BackgroundPids[i],&ChildStatus,WNOHANG);
+
+        if(ChildStatus == 0 || ChildStatus == 1){ // if exited or errored on exit.
+            fprintf(stdout,"\nBackground pid %d is done: exit value %d\n",ChildPid,ChildStatus);
+            RemoveBackPid(ChildPid);
+            //PopBackPid();
+        }
+        else{ // other signals.
+            fprintf(stdout,"\nBackground pid %d is done: terminated by signal %d\n", ChildPid, ChildStatus);
+            RemoveBackPid(ChildPid);
+            //PopBackPid();
+        }
+    }
+    
+}
+
+/// NAME: TrapChildSignal
+/// DESC: signal handler for ^c of a fg process.
 void TrapTermSignal(int sig)
 {
     printf("\nterminated by signal %d\n",sig); 
@@ -340,14 +391,36 @@ void TrapTermSignal(int sig)
     fflush(stdin);  
 }
 
+/// NAME: FreeAndClearInputObj
+/// DESC: Helper thats frees input object.
 void FreeAndClearInputObj(struct InputObj* Obj)
 {
+    //int i;
+
+    Obj->Background = false; // reset bg
+    //clear all fields.
     memset(Obj->InputFile,'\0',sizeof(Obj->InputFile));
     memset(Obj->OutputFile,'\0',sizeof(Obj->OutputFile));
     memset(Obj->Command,'\0',sizeof(Obj->Command));
+
+    // for(i = 0; i < Obj->NumArgs;i++){
+    //     memset(Obj->Arguments[i],'\0',sizeof(Obj->Arguments[i]) );
+    // }
+
     free(Obj);//IMPORTANT WITHOUT THIS SIGABRT SIGNAL WILL BE TRIGGERED <<<<<<<<
 }
 
+/// NAME: KillBGProcesses
+/// DESC: helper function for exting.
+void KillBGProcesses()
+{
+    int i;
+    for(i = 0;i < PidStack.NumBackPid + 1;i++){
+        kill(PidStack.BackgroundPids[i], SIGINT); // interrupt all bg pids.
+    }
+}
+/// NAME: RunShell
+/// DESC: runs the shell itself.
 void RunShell()
 {
     char InputBuffer[1028];
@@ -362,53 +435,56 @@ void RunShell()
     struct sigaction TermSignal;
     TermSignal.sa_handler = TrapTermSignal;
     StopSignal.sa_flags = 0;
+
+    struct sigaction ChildSignal;
+    ChildSignal.sa_handler = TrapChildSignal;
+    StopSignal.sa_flags = 0;
     //end init signals
 
     do
     {
-        sigaction(SIGTSTP,&StopSignal,NULL);
-        sigaction(SIGINT,&TermSignal,NULL);
-        
+        //reseting signal handlers.
+        sigaction(SIGTSTP,&StopSignal, NULL);
+        sigaction(SIGINT,&TermSignal, NULL);
+        sigaction(SIGCHLD,&ChildSignal, NULL);
 
-        if(_BackGroundPidCompleted() == true){
-            continue;
-        }
 
+        //clearing stdin and out.
         fflush(stdout);
         fflush(stdin);
 
         printf(": ");
-        fgets(InputBuffer,1028,stdin);
+        fgets(InputBuffer,1028,stdin); // get command line.
         
         fflush(stdout);
         fflush(stdin);
 
-        if(strncmp(InputBuffer,"exit",4) == 0){
-            printf("Exiting....\n");
-            return;
+        if(strncmp(InputBuffer,"exit",4) == 0){ // exit shell.
+            //printf("Exiting....\n");
+            KillBGProcesses();
+            exit(0);
         }
-        else if(strncmp(InputBuffer, "#",1) == 0){
+        else if(strncmp(InputBuffer, "#",1) == 0){ // comment.
             //printf("Comment Comment Comment \n");
         }
-        else if(strncmp(InputBuffer,"cd ", 3) == 0){
+        else if(strncmp(InputBuffer,"cd ", 3) == 0){ // change directory
             //printf("Changing directory...\n");
             ChangeDirectorysFromHome(InputBuffer);
         }
-        else if(strncmp(InputBuffer,"status",6) == 0){
+        else if(strncmp(InputBuffer,"status",6) == 0){ // check last fg command status.
             if(WEXITSTATUS(LastForeGroundStatus)){
-                ForegroundStatus = WEXITSTATUS(LastForeGroundStatus);
+                ForegroundStatus = WEXITSTATUS(LastForeGroundStatus); // check if extied.
             }
             else{
-                ForegroundStatus = WTERMSIG(LastForeGroundStatus);
+                ForegroundStatus = WTERMSIG(LastForeGroundStatus); // check if terminated by signal.
             }
-
             printf("exit value %d\n",ForegroundStatus);
         }
         else{
             //read in a command.
             Obj = malloc(1 * sizeof(struct InputObj)); 
-            ParseInput(InputBuffer,Obj);
-            RunCommand(Obj);
+            ParseInput(InputBuffer,Obj); // parse command line.
+            RunCommand(Obj); // run ocmmand.
 
             FreeAndClearInputObj(Obj);
         }
@@ -416,9 +492,11 @@ void RunShell()
     while(true);
 }
 
+/// NAME: main
+/// DESC: RUNS THIS THING.
 int main(void)
 {
     _InitPidObj();
-    RunShell();
+    RunShell(); // singleton for shell.
     return 0;
 }
